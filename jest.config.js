b6eefcda273d2c39d7a67ff08d4c8a64a832acc4
Lakshaya Sood <lakshaya.sood@infosys.com>
2019-12-07 12:56:37 +0100

    🛠build (configs update): Update ts, jest &  git config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  "collectCoverage": true,
+  "coverageDirectory": "<rootDir>/tests/unit/coverage",
+  "collectCoverageFrom": ["src/**/*.{ts,vue}", "!src/assets/**/*", "!src/*", "!src/router/*", "!src/store/index.ts"]
 };
